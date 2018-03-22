253c7242154e742d91ca63cba991ee9794b3325c
Sam <mooreman9@gmail.com>
2018-03-22 05:41:29 +0000

    Moving Code to GUI (in progress)

--- switch_tester_relay_rfid_GUI.py
+++ switch_tester_relay_rfid_GUI.py
@@ -21,14 +21,25 @@ class Application(Frame):
         self.locked = True
 
         #Chip sensors
+        self.reader = SimpleMFRC522.SimpleMFRC522()
         
         #Sound setup
         pygame.init()
         pygame.mixer.init()
         self.altar = pygame.mixer.Sound('put-staff-in-altar_temp.wav')
+        self.error = pygame.mixer.Sound('failure-short_temp.wav')
 
         self.create_widgets()
         self.ChipCheck()
+
+        #Mag-lock setup
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        GPIO.setup(2, GPIO.OUT)
+        GPIO.setup(3, GPIO.OUT)
+        GPIO.output(2, GPIO.LOW)
+        GPIO.output(3, GPIO.LOW)
+        
         
         
     def create_widgets(self):
@@ -61,7 +72,64 @@ class Application(Frame):
         self.bttnum5.grid(row = 1, column = 4)
         
     def ChipCheck(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        
+        GPIO.setup(2, GPIO.OUT)
+        GPIO.setup(3, GPIO.OUT)
 
+        GPIO.setup(23, GPIO.IN, GPIO.PUD_UP)
+        GPIO.setup(17, GPIO.IN, GPIO.PUD_UP)
+        GPIO.setup(27, GPIO.IN, GPIO.PUD_UP)
+        GPIO.setup(22, GPIO.IN, GPIO.PUD_UP)
+
+        loop = '1'
+        button1 = GPIO.input(23)
+        GPIO.output(2, GPIO.LOW)
+        GPIO.output(3, GPIO.LOW)
+
+        while loop == '1':
+            num = ''
+            id, text = reader.read()
+
+            if id == 742561995189:
+                 num = '1'
+                 GPIO.output(2, GPIO.HIGH)
+                 self.altar.Play()
+                 time.sleep(1)
+                 loop = '2'
+
+        while loop == '2':
+            input_one = GPIO.input(23)
+            input_two = GPIO.input(17)
+            input_three = GPIO.input(27)
+            input_four = GPIO.input(22)
+            time.sleep(.5)
+            if input_one == False:
+                self.altar.Play()
+                GPIO.output(3, GPIO.HIGH)
+                time.sleep(1)
+                loop = '3'
+            if input_two == False:
+                self.error.Play()
+                time.sleep(.5)
+            if input_three == False:
+                self.error.Play()
+                time.sleep(.5)
+            if input_four == False:
+                self.error.Play()
+                time.sleep(.5)
+        while loop == '3':
+            id, text = reader.read()
+            print(id)
+            print(text)
+
+            if id == 552222697650:
+                GPIO.output(2, GPIO.LOW)
+                GPIO.output(3, GPIO.LOW)
+                loop = '1'    
+
+        
         self.after(250, self.ChipCheck)
         
     def PressButton(self, val):
